c4325212a04d02a2369f8ce9afa06fd5d2178caf
RafKik <107320608+rafkik@users.noreply.github.com>
2024-03-24 12:58:12 +0000

    Update main.py.

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
-from telebot import TeleBot, InlineKeyboardMarkup
+from telebot import TeleBot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import fishes
 
 API_TOKEN = '6779226413:AAEOnToeoYGYfOE_A7DnzJ836_N4MmRSPaA'
 
 bot = TeleBot(API_TOKEN)
 
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = InlineKeyboardMarkup()
-    markup.add(
-        f'Искать',
-        callback_data='search'
-    )
+    search = InlineKeyboardButton(f'Искать', callback_data='search')
+    markup.row(search)
     bot.send_message(
         message.chat.id,
-        f'Привет, в этом боте ты можешь найти информацию о рыбах которые обитают в Беларуси, где их ловить и другую информацию.',
+        f'Привет, в данном боте ты можешь найти информацию о рыбах Беларуси.',
         reply_markup=markup
     )
 
 
 @bot.callback_query_handler(func=lambda callback: True)
-def callback_query_handler(callback):
+def get_callback(callback):
     if callback.data == 'search':
-        bot.send_message(callback.id, "Вы нажали Кнопку 1")
+        bot.send_message(callback.message.chat.id, f'Введите название рыбы:')
+        bot.register_next_step_handler(callback.message, search)
 
 
-@bot.message_handler()
-def echo(message):
-    bot.send_message(
-        message.chat.id,
-        message.text
-    )
+def search(message):
+    if message.text.lower() == 'окунь':
+        bot.send_message(message.chat.id, fishes.perch)
 
 
 bot.infinity_polling()
